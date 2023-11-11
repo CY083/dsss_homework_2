79de9697e13ebf290d7581a6f9edc3f173ad883b
CY083 <ckoehn@t-online.de>
2023-11-11 20:30:55 +0100

    Task 03 and 04

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,42 +1,79 @@
 import random
 
 
-def function_A(min, max):
+def get_random_int(min, max):
     """
-    Random integer.
+    Create a random integer from the interval [min, max].
+
+    Args:
+        min (int): The minimum integer.
+        max (int): The maximum integer.
+
+    Returns:
+        int: A random int.
+
     """
     return random.randint(min, max)
 
 
-def function_B():
+def get_random_operator():
+    """
+    Get a randomized operator from the choice of {+, -, *}
+
+    Returns: 
+        str: An operator.
+    
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
+def calculate(n1, n2, o):
+    """
+    Calculates a term.
+
+    Args:
+        n1 (int): The first number.
+        n2 (int): The second number.
+        o (str): An operator.
+    
+    Returns:
+        str: The term as string.
+        int: The result of the calculation.
+    """
+
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
+    if o == '+':
+        a = n1 + n2
+    elif o == '-':
+        a = n1 - n2
+    else: 
+        a = n1 * n2
     return p, a
 
 def math_quiz():
     s = 0
-    t_q = 3.14159265359
+    t_q = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = get_random_int(1, 10); 
+        n2 = get_random_int(1, 5); 
+        o = get_random_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = calculate(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
 
+        try:
+            useranswer = input("Your answer: ")
+            useranswer = int(useranswer)
+        except:
+            print("Input an integer!")
+            
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            s += 1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
