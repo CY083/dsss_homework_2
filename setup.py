3a6bf3e06d703798288531be7f1270badbff39d2
CY083 <ckoehn@t-online.de>
2023-11-11 21:19:09 +0100

    Completed all tasks, made package pip installable.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mathquiz",
+    version="0.1",
+    author="se49fama",
+    author_email="se49fama@fau.de",
+    packages=find_packages(),
+    install_requires=["random", "unittest"],
+)
